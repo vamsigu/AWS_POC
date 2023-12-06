da45995125bb21ac02719a5a7c309bf294460927
GUDE VAMSI <116723956+vamsigu@users.noreply.github.com>
2023-12-06 11:18:08 +0530

    Add files via upload

---
+++ Kalibr8Aws_Pocs/Api start_stop/Controllers/Function.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Amazon.ComputeOptimizer;
+using Amazon.ComputeOptimizer.Model;
+
+namespace Apistart_stop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EC2Controller : ControllerBase
+    {
+        private readonly ILogger<EC2Controller> _logger;
+
+        public EC2Controller(ILogger<EC2Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("get-instances")]
+        public async Task<IActionResult> GetEC2Instances([FromBody] RequestModel requestModel)
+        {
+            try
+            {
+                var ec2Client = new AmazonEC2Client(requestModel.AWSAccessKey, requestModel.AWSSecretKey, RegionEndpoint.GetBySystemName(requestModel.Region));
+
+                var request = new DescribeInstancesRequest();
+                var response = await ec2Client.DescribeInstancesAsync(request);
+
+                var instances = new List<Instance>();
+
+                foreach (var reservation in response.Reservations)
+                {
+                    instances.AddRange(reservation.Instances);
+                }
+
+                return Ok(instances);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error: {ex.Message}");
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("start-all-instances")]
+        public async Task<IActionResult> StartAllEC2Instances([FromBody] RequestModel requestModel)
+        {
+            try
+            {
+                var ec2Client = new AmazonEC2Client(requestModel.AWSAccessKey, requestModel.AWSSecretKey, RegionEndpoint.GetBySystemName(requestModel.Region));
+
+                var instanceIds = await GetInstanceIdsAsync(ec2Client);
+
+                var request = new StartInstancesRequest
+                {
+                    InstanceIds = instanceIds
+                };
+
+                var response = await ec2Client.StartInstancesAsync(request);
+
+                return Ok(response.StartingInstances);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error: {ex.Message}");
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("stop-all-instances")]
+        public async Task<IActionResult> StopAllEC2Instances([FromBody] RequestModel requestModel)
+        {
+            try
+            {
+                var ec2Client = new AmazonEC2Client(requestModel.AWSAccessKey, requestModel.AWSSecretKey, RegionEndpoint.GetBySystemName(requestModel.Region));
+
+                var instanceIds = await GetInstanceIdsAsync(ec2Client);
+
+                var request = new StopInstancesRequest
+                {
+                    InstanceIds = instanceIds
+                };
+
+                var response = await ec2Client.StopInstancesAsync(request);
+
+                return Ok(response.StoppingInstances);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error: {ex.Message}");
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("get-recommendations")]
+        public async Task<IActionResult> GetEC2Recommendations([FromBody] RequestModel requestModel)
+        {
+            try
+            {
+                var ec2Client = new AmazonEC2Client(requestModel.AWSAccessKey, requestModel.AWSSecretKey, RegionEndpoint.GetBySystemName(requestModel.Region));
+
+                var instanceId = await GetInstanceIdsAsync(ec2Client);
+
+
+                var computeOptimizerClient = new AmazonComputeOptimizerClient(requestModel.AWSAccessKey, requestModel.AWSSecretKey, RegionEndpoint.GetBySystemName(requestModel.Region));
+
+                var getRecommendationsRequest = new GetEC2InstanceRecommendationsRequest
+                {
+                    InstanceArns = new List<string> { $"arn:aws:ec2:{requestModel.Region}:{requestModel.Account_id}:instance/{instanceId}" }
+                };
+
+                var recommendationsResponse = await computeOptimizerClient.GetEC2InstanceRecommendationsAsync(getRecommendationsRequest);
+
+                return Ok(recommendationsResponse);
+            }
+            catch (AmazonComputeOptimizerException ex)
+            {
+                _logger.LogError($"AWS Compute Optimizer Error: {ex.Message}");
+                return BadRequest($"AWS Compute Optimizer Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error: {ex.Message}");
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+        private static async Task<List<string>> GetInstanceIdsAsync(IAmazonEC2 ec2Client)
+        {
+            var request = new DescribeInstancesRequest();
+            var response = await ec2Client.DescribeInstancesAsync(request);
+
+            var instanceIds = new List<string>();
+
+            foreach (var reservation in response.Reservations)
+            {
+                foreach (var instance in reservation.Instances)
+                {
+                    instanceIds.Add(instance.InstanceId);
+                }
+            }
+
+            return instanceIds;
+        }
+    }
+
+}
---
+++ Kalibr8Aws_Pocs/Api start_stop/RequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Apistart_stop
+{
+    public class RequestModel
+    {
+         public string AWSAccessKey { get; set; }
+         public string AWSSecretKey { get; set; }
+         public string Region { get; set; }
+         public string Account_id { get; set; }
+    }
+}
